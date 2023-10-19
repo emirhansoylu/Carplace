ca66fd9f1244a7aa7c2ac776aefa6e8eeb2a136e
Emirhan Soylu <emirhansoylu98@gmail.com>
2023-10-19 20:05:34 +0300

    Project created

---
+++ app/src/main/java/dev/duckbuddyy/carplace/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.duckbuddyy.carplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
